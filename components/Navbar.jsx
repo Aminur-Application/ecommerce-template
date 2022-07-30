efb427975eea488c3372729a5d574546dbaa9cc3
Prophecyuntold <aminur999@hotmail.com>
2022-07-31 00:06:03 +0100

    ecommerce-template

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href="/">GoNatural</Link>
+        <Link href="/">Ecommerce-template</Link>
       </p>
       <button type="button" className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
